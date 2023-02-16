ec92b7ba6aaa5e201b7e99feee72dced3a830c44
Geoff Little <g.little712@gmail.com>
2023-02-15 20:43:07 -0600

    GETing public symphony edns

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
-const main = () => {
-  console.log("Hello world!");
+import axios from "axios";
+import {parseEDNString} from "edn-data";
+
+const sleep = async (ms: number) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
 };
 
-main();
+const main = async () => {
+  const res = await axios.get("https://firestore.googleapis.com/v1/projects/leverheads-278521/databases/(default)/documents/symphony/rS2JIupnK25fu6W04W4a");
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  // console.log(res.data);
+
+  const k: any = parseEDNString(res.data.fields.latest_version_edn.stringValue);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  console.log(k.map[6][1][0].map);
+};
+
+void (async () => await main())();
